2d69418cbee185179c8eff96e1db6ba0423afdc3
JUN2973 <wnsdud2973@gmail.com>
2026-08-27 10:48:41 +0900

    Hello World

---
+++ 2-1/hello.go
@@ -0,0 +1,17 @@
+// hello
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+var i int
+var s string
+var age int = 10
+var name string = "Maria"
+
+// 2-1 Á¤¸®
